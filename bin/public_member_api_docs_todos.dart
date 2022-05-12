36dc6e7aebe9c7390f8f26d085a8acec58290c38
Rexios <rexios@rexios.dev>
2022-05-12 18:19:56 -0400

    Initial commit

---
+++ bin/public_member_api_docs_todos.dart
@@ -0,0 +1,5 @@
+import 'package:public_member_api_docs_todos/public_member_api_docs_todos.dart' as public_member_api_docs_todos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${public_member_api_docs_todos.calculate()}!');
+}
---
+++ test/public_member_api_docs_todos_test.dart
@@ -0,0 +1,8 @@
+import 'package:public_member_api_docs_todos/public_member_api_docs_todos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
